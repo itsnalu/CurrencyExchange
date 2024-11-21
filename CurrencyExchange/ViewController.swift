c8a2f2ee2f1f15d616a2d385ae2795b397e829cf
Ana Luísa Moreira <ana.luisa.rodrigues@ufv.br>
2024-11-20 21:20:16 -0300

    Initial Commit

---
+++ CurrencyExchange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyExchange
+//
+//  Created by Ana Luisa Moreira Rodrigues on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CurrencyExchangeTests/CurrencyExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeTests.swift
+//  CurrencyExchangeTests
+//
+//  Created by Ana Luisa Moreira Rodrigues on 20/11/24.
+//
+
+import Testing
+@testable import CurrencyExchange
+
+struct CurrencyExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
